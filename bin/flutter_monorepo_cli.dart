b3dfa48ba3dfc074bd2c0470a360f5aab8626b6a
Nirmal Ariyathilake <nirmal.ariyathilake@gmail.com>
2024-01-02 23:56:32 +0530

    Initial commit

---
+++ bin/flutter_monorepo_cli.dart
@@ -0,0 +1,5 @@
+import 'package:flutter_monorepo_cli/flutter_monorepo_cli.dart' as flutter_monorepo_cli;
+
+void main(List<String> arguments) {
+  print('Hello world: ${flutter_monorepo_cli.calculate()}!');
+}
---
+++ test/flutter_monorepo_cli_test.dart
@@ -0,0 +1,8 @@
+import 'package:flutter_monorepo_cli/flutter_monorepo_cli.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
